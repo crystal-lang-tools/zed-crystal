ff8d4942171af683425ea64b789e50977e9ec39e
Margret Riegert <margret@eowyn.net>
2025-02-08 20:34:17 -0500

    Update to latest tree-sitter-crystal Macro injections

--- languages/crystal/injections.scm
+++ languages/crystal/injections.scm
@@ -1,4 +1,36 @@
 (heredoc_body
-  (heredoc_content) @content
-  (heredoc_end) @language
-  (#downcase! @language))
+  (heredoc_content) @injection.content
+  (heredoc_end) @injection.language
+  (#downcase! injection.language))
+
+(macro_def
+  body: (expressions) @injection.content
+  (#set! injection.language "crystal"))
+
+(macro_begin
+  body: (expressions) @injection.content
+  (#set! injection.language "crystal"))
+
+(macro_if
+  then: (expressions) @injection.content
+  (#set! injection.language "crystal"))
+
+(macro_elsif
+  then: (expressions) @injection.content
+  (#set! injection.language "crystal"))
+
+(macro_else
+  body: (expressions) @injection.content
+  (#set! injection.language "crystal"))
+
+(macro_unless
+  then: (expressions) @injection.content
+  (#set! injection.language "crystal"))
+
+(macro_for
+  body: (expressions) @injection.content
+  (#set! injection.language "crystal"))
+
+(macro_verbatim
+  body: (expressions) @injection.content
+  (#set! injection.language "crystal"))
